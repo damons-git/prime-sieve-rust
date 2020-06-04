0e67c66b9246bc2f8c6f605206a108eb8a9cfb03
Damon Sweeney <me@damons.email>
2020-06-04 18:11:32 +0100

    Add primitive implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+mod vector_fn;
+
+/**
+ * Sieve of Eratosthenes for primality testing.
+ *
+ * For a number to be considered prime it must be only
+ * divisible by itself and one.
+ *
+ * Note: The above criteria does not hold for the value 1,
+ * as it is only divisible by itself.
+ */
+
 fn main() {
-    println!("Hello, world!");
+    let res: Vec<i32> = prime_sieve(100);
+    println!("{:?}", res);
 }
+
+fn prime_sieve(limit: i32) -> Vec<i32> {
+    // Generate vector containing numbers 2 through to limit.
+    let mut sieve: Vec<i32> = Vec::new();
+    for x in 2..(limit+1) {
+        sieve.push(x);
+    }
+
+    //
+    let mut invalid: Vec<i32> = Vec::new();
+    for x in sieve.iter() {
+        let mut multiple = *x;
+        while multiple < limit {
+            multiple += *x;
+            if !invalid.contains(&multiple) {
+                invalid.push(multiple);
+            }
+        }
+    }
+
+    let res: Vec<i32> = vector_fn::diff(sieve, invalid);
+    return res;
+}
+
+
